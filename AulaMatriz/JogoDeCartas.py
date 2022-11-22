32148be577ecdcf22ea1ec52d737eb659667001e
Maria Antonia Galdino <107472413+MariaAntoniaGaldino@users.noreply.github.com>
2022-11-21 22:59:39 -0300

    Add files via upload

---
+++ AulaMatriz/JogoDeCartas.py
@@ -0,0 +1,68 @@
+A=[A1,A2,A3,A4,A5,A6,A7]
+B=[B1,B2,B3,B4,B5,B6,B7]
+C=[C1,C2,C3,C4,C5,C6,C7]
+
+
+for i in range(7)
+    print(A[i] + ' ' + B[i] + ' ' C[i])
+
+comando = input('Digite a coluna:')
+
+if comando == 'A':
+       lI = B + A + C
+if comando == 'B':
+       lI = A + B + C
+if comando == 'C':
+       lI = A + C + B
+
+
+A= []
+B= []
+C= []
+
+
+for i in range(7)
+    A.append(lI[0])
+    B.append(lI[1])
+    C.append(lI[2])
+  
+print(lI)
+
+
+
+
+# lista= [A]
+# lista= [B]
+# Lista= [C]
+
+  
+#   if comando == 'A':
+
+
+
+# for i in range(A)
+# lista.append(i)
+
+
+# for i in range(B)
+# listaA.append(i)
+
+
+# for i in range(C)
+# listaB.append(i)
+
+
+# listaA = listaB + listaC
+# print(i)
+
+
+# A+B+C = float(input('digite a coluna escolhida:'))
+# print(a+b+C)
+
+
+
+
+
+
+
+
